acef79dc9f9b2736b4fd2ba5f1518ac3d4651a26
Brandon Miller <brandonmiller@live.ca>
2017-05-28 19:27:51 -0600

    basic command

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,18 @@
 import discord
 import asyncio
 
-client=discord.client()
+client = discord.Client()
 
 @client.event
-async
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+       await client.send_message(message.channel, 'Pong!')
+
+client.run('MzE4NTAyMTg4MDM1MDgwMTky.DA0HEQ.wa9Tgwh2tSTGwiMUCs8G98UWvuE')
